5b5af4ad14b74096888b72c32dedf4821c6f0cea
Tales de Carvalho <tales.fsa@hotmail.com>
2024-08-13 21:33:35 -0300

    Title mudado

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $entretenimento = buscaEntretenimentos($con);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Entretenimento</title>
 </head>
 
 <body>
